cc372d3f6aadca6f8dbec1a86a2b7adb4b1f0a87
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-09-20 02:04:09 +0500

    first commit

--- bd-connect.go
+++ bd-connect.go
@@ -1,3 +1,23 @@
+// package main
+
+// import (
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var urlDns="root:ilove1382005#@Tcp(localhost:3306)/mysqldb";
+// var Database  *gorm.DB
+// var err error
+
+// func DataMigration()  {
+
+// 	Database, err = gorm.Open(mysql.Open(urlDns),&gorm.Config{});
+// 	if err!=nil{
+//         panic("Failed to connect to database"+error.Error(err))
+//     }
+// 	Database.AutoMigrate(&Employee{})
+// }
+
 package main
 
 import (
@@ -5,16 +25,17 @@ import (
 	"gorm.io/gorm"
 )
 
-var urlDns="root:ilove1382005#@Tcp(localhost:3306)/mysqldb";
-var Database  *gorm.DB
-var err error
+var Database *gorm.DB;
+
+var urlDatabase  ="root:ilove1382005#@Tcp(localhost:3306)/mysqldb";
+
+var err error;
 
 
 func DataMigration()  {
-	
-	Database, err = gorm.Open(mysql.Open(urlDns),&gorm.Config{});
-	if err!=nil{
-        panic("Failed to connect to database"+error.Error(err))
-    }
+	Database,err =gorm.Open(mysql.Open(urlDatabase),&gorm.Config{})
+	if err != nil {
+		panic("Failed To Connection to Database please cheak Abdullah"+err.Error())
+	}
 	Database.AutoMigrate(&Employee{})
 }
--- employ-hand.go
+++ employ-hand.go
@@ -1,8 +1,21 @@
 package main
-import  "net/http"
+
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func CreateEmployee(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-type","application/json");
+	var empp Employee;
+	json.NewDecoder(r.Body).Decode(empp);
+	json.NewEncoder(w).Encode(empp);
+	//db.Create(&empp);
+	Database.Create(&empp)
+	//fmt.Fprintf(w, "New Employee: %+v", empp)
+	json.NewEncoder(w).Encode(&empp)
+
+
 }
 func GetEmployee(w http.ResponseWriter, r *http.Request) {
 	
